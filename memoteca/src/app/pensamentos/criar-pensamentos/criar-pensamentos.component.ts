5eea4bd96ace9453597973fc642fc3dec9e092a6
henrique-a-santos <henriqe.e2@gmail.com>
2022-12-07 16:15:52 -0300

    refator

---
+++ memoteca/src/app/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamentos',
+  templateUrl: './criar-pensamentos.component.html',
+  styleUrls: ['./criar-pensamentos.component.css']
+})
+export class CriarPensamentosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
